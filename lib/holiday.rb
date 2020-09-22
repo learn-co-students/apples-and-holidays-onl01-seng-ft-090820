b3e01236c4e6c299e27448164ef55010b52dd134
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 02:23:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,3 +51,68 @@ require 'pry'
 #   end.flatten.compact
 # end
 # all_holidays_with_bbq(holiday_supplies)
+
+holiday_hash[:summer][:fourth_of_july][1]
+end
+
+
+def add_supply_to_winter_holidays(holiday_hash, supply)	def add_supply_to_winter_holidays(holiday_hash, supply)
+
+
+  holiday_hash[:winter].each {|k, v| holiday_hash[:winter][k] << supply }
+    end
+end
+
+
+def add_supply_to_memorial_day(holiday_hash, supply)
+
+  holiday_hash[:spring][:memorial_day] << supply
+end	end
+
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)	def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+
+  holiday_hash[season][holiday_name] = supply_array
+end	end
+
+
+def all_winter_holiday_supplies(holiday_hash)	def all_winter_holiday_supplies(holiday_hash)
+  
+  holiday_hash[:winter].values.flatten
+end	end
+
+
+def all_supplies_in_holidays(holiday_hash)
+
+
+  holiday_hash.each do |season, data|
+    puts "#{season.to_s.capitalize!}:"
+    data.each do |holiday, supply|
+      array = holiday.to_s.split("_")
+      final_holiday = []
+        array.each do |x|
+          final_holiday << x.capitalize!
+        end
+      holiday = final_holiday.join(" ")
+
+      supply = supply.join(", ")
+      puts "  #{holiday}: #{supply}"
+
+    end
+  end
+
+def all_holidays_with_bbq(holiday_hash)	def all_holidays_with_bbq(holiday_hash
+
+answer = []
+holiday_hash.each do |season, data|
+  data.each do |holiday, value|
+    value.each do |supply|
+     if supply == "BBQ"
+        answer.push(holiday)
+      end
+    end
+  end
+end
+answer
+
+end
